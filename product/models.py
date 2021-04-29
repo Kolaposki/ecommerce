a64e4c2fc01f0c84f8515d463dd9f49fd8d195ee
Oshodi Kolapo <oshodikolapo@gmail.com>
2021-04-29 04:50:39 +0100

    :hammer: :zap: Refactor codes

--- product/models.py
+++ product/models.py
@@ -18,18 +18,15 @@ class Category(MPTTModel):
         ('True', 'True'),
         ('False', 'False'),
     )
-    parent = TreeForeignKey('self',blank=True, null=True ,related_name='children', on_delete=models.CASCADE)
+    parent = TreeForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     keywords = models.CharField(max_length=255)
     description = models.TextField(max_length=255)
-    image=models.ImageField(blank=True,upload_to='images/')
-    status=models.CharField(max_length=10, choices=STATUS)
+    image = models.ImageField(blank=True, upload_to='images/')
+    status = models.CharField(max_length=10, choices=STATUS)
     slug = models.SlugField(null=False, unique=True)
-    create_at=models.DateTimeField(auto_now_add=True)
-    update_at=models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.title
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
 
     class MPTTMeta:
         order_insertion_by = ['title']
@@ -37,8 +34,8 @@ class Category(MPTTModel):
     def get_absolute_url(self):
         return reverse('category_detail', kwargs={'slug': self.slug})
 
-    def __str__(self):                           # __str__ method elaborated later in
-        full_path = [self.title]                  # post.  use __unicode__ in place of
+    def __str__(self):  # __str__ method elaborated later in
+        full_path = [self.title]  # post.  use __unicode__ in place of
         k = self.parent
         while k is not None:
             full_path.append(k.title)
@@ -59,23 +56,23 @@ class Product(models.Model):
         ('Size-Color', 'Size-Color'),
 
     )
-    category = models.ForeignKey(Category, on_delete=models.CASCADE) #many to one relation with Category
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # many to one relation with Category
     title = models.CharField(max_length=150)
     keywords = models.CharField(max_length=255)
     description = models.TextField(max_length=255)
-    image=models.ImageField(upload_to='images/',null=False)
-    price = models.DecimalField(max_digits=12, decimal_places=2,default=0)
-    amount=models.IntegerField(default=0)
-    minamount=models.IntegerField(default=3)
-    variant=models.CharField(max_length=10,choices=VARIANTS, default='None')
-    detail=RichTextUploadingField()
+    image = models.ImageField(upload_to='images/', null=False)
+    price = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    amount = models.IntegerField(default=0)
+    minamount = models.IntegerField(default=3)
+    variant = models.CharField(max_length=10, choices=VARIANTS, default='None')
+    detail = RichTextUploadingField()
     slug = models.SlugField(null=False, unique=True)
-    status=models.CharField(max_length=10,choices=STATUS)
-    create_at=models.DateTimeField(auto_now_add=True)
-    update_at=models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.title
-
 
     ## method to create a fake table field in read only mode
     def image_tag(self):
@@ -89,26 +86,27 @@ class Product(models.Model):
 
     def avaregereview(self):
         reviews = Comment.objects.filter(product=self, status='True').aggregate(avarage=Avg('rate'))
-        avg=0
+        avg = 0
         if reviews["avarage"] is not None:
-            avg=float(reviews["avarage"])
+            avg = float(reviews["avarage"])
         return avg
 
     def countreview(self):
         reviews = Comment.objects.filter(product=self, status='True').aggregate(count=Count('id'))
-        cnt=0
+        cnt = 0
         if reviews["count"] is not None:
             cnt = int(reviews["count"])
         return cnt
 
 
 class Images(models.Model):
-    product=models.ForeignKey(Product,on_delete=models.CASCADE)
-    title = models.CharField(max_length=50,blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, blank=True)
     image = models.ImageField(blank=True, upload_to='images/')
 
     def __str__(self):
         return self.title
+
 
 class Comment(models.Model):
     STATUS = (
@@ -116,18 +114,19 @@ class Comment(models.Model):
         ('True', 'True'),
         ('False', 'False'),
     )
-    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     subject = models.CharField(max_length=50, blank=True)
-    comment = models.CharField(max_length=250,blank=True)
+    comment = models.CharField(max_length=250, blank=True)
     rate = models.IntegerField(default=1)
     ip = models.CharField(max_length=20, blank=True)
-    status=models.CharField(max_length=10,choices=STATUS, default='New')
-    create_at=models.DateTimeField(auto_now_add=True)
-    update_at=models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS, default='New')
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.subject
+
 
 class CommentForm(ModelForm):
     class Meta:
@@ -137,9 +136,11 @@ class CommentForm(ModelForm):
 
 class Color(models.Model):
     name = models.CharField(max_length=20)
-    code = models.CharField(max_length=10, blank=True,null=True)
+    code = models.CharField(max_length=10, blank=True, null=True)
+
     def __str__(self):
         return self.name
+
     def color_tag(self):
         if self.code is not None:
             return mark_safe('<p style="background-color:{}">Color </p>'.format(self.code))
@@ -149,18 +150,20 @@ class Color(models.Model):
 
 class Size(models.Model):
     name = models.CharField(max_length=20)
-    code = models.CharField(max_length=10, blank=True,null=True)
+    code = models.CharField(max_length=10, blank=True, null=True)
+
     def __str__(self):
         return self.name
 
+
 class Variants(models.Model):
-    title = models.CharField(max_length=100, blank=True,null=True)
+    title = models.CharField(max_length=100, blank=True, null=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    color = models.ForeignKey(Color, on_delete=models.CASCADE,blank=True,null=True)
-    size = models.ForeignKey(Size, on_delete=models.CASCADE,blank=True,null=True)
-    image_id = models.IntegerField(blank=True,null=True,default=0)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE, blank=True, null=True)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE, blank=True, null=True)
+    image_id = models.IntegerField(blank=True, null=True, default=0)
     quantity = models.IntegerField(default=1)
-    price = models.DecimalField(max_digits=12, decimal_places=2,default=0)
+    price = models.DecimalField(max_digits=12, decimal_places=2, default=0)
 
     def __str__(self):
         return self.title
@@ -168,39 +171,43 @@ class Variants(models.Model):
     def image(self):
         img = Images.objects.get(id=self.image_id)
         if img.id is not None:
-             varimage=img.image.url
+            varimage = img.image.url
         else:
-            varimage=""
+            varimage = ""
         return varimage
 
     def image_tag(self):
         img = Images.objects.get(id=self.image_id)
         if img.id is not None:
-             return mark_safe('<img src="{}" height="50"/>'.format(img.image.url))
+            return mark_safe('<img src="{}" height="50"/>'.format(img.image.url))
         else:
             return ""
 
-llist= Language.objects.all()
-list1=[]
+
+llist = Language.objects.all()
+list1 = []
 for rs in llist:
-    list1.append((rs.code,rs.name))
-langlist= (list1)
+    list1.append((rs.code, rs.name))
+langlist = (list1)
+
 
 class ProductLang(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE) #many to one relation with Category
-    lang =  models.CharField(max_length=6, choices=langlist)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)  # many to one relation with Category
+    lang = models.CharField(max_length=6, choices=langlist)
     title = models.CharField(max_length=150)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     slug = models.SlugField(null=False, unique=True)
-    detail=RichTextUploadingField()
+    detail = RichTextUploadingField()
 
     def get_absolute_url(self):
         return reverse('product_detail', kwargs={'slug': self.slug})
 
+
 class CategoryLang(models.Model):
-    category = models.ForeignKey(Category, related_name='categorylangs', on_delete=models.CASCADE) #many to one relation with Category
-    lang =  models.CharField(max_length=6, choices=langlist)
+    category = models.ForeignKey(Category, related_name='categorylangs',
+                                 on_delete=models.CASCADE)  # many to one relation with Category
+    lang = models.CharField(max_length=6, choices=langlist)
     title = models.CharField(max_length=150)
     keywords = models.CharField(max_length=255)
     slug = models.SlugField(null=False, unique=True)
