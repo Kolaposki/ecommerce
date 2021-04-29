42c9430c5cabc63233c91266b601547974ee0b28
Oshodi Kolapo <oshodikolapo@gmail.com>
2021-04-29 03:50:17 +0100

    :hammer: Refactor codes

--- todo.js
+++ todo.js
@@ -1,7 +1,7 @@
 // TODO : A js plugin to zoom pic when hover
 // TODO : Desgin login page
 // TODO : Desgin registration page
-// TODO :
+// TODO : Compare two products
 // TODO :
 // TODO :
 // TODO :
